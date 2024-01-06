c1a0d697a191b8a024087351d4c9627e78edee89
goldenonraisl <adriansantosrollim@gmail.com>
2024-01-06 10:55:57 -0200

    mudando de servidor para o hostweb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("node:http");
 
-const hostname = '127.0.0.1';
+const hostname = 'app-js-mauve.vercel.app';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}`);
   });
